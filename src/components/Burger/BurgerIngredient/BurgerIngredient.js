29f747d2dca1117af516161e764eed1fb71d6ec4
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-23 21:39:03 +0100

    rendered BurgerIngredients in Burger component + added state for BurgerBuilder component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from "props-type";
+import PropTypes from "prop-types";
 
 import classes from "./BurgerIngredient.css";
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+
 import Aux from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
