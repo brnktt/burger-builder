58a430fdb6d56d743f51cbffdcbf47fe69921312
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-23 19:45:05 +0100

    installed prop-types dependency + created BurgerIngredient component with styling + create basic BurgerBuider component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.css";
 
 const layout = props => {
   return (
     <Aux>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
 };
