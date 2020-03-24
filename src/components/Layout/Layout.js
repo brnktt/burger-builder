4660539c743b518c459ed7ab9fd54fa9c2ad5169
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-25 00:44:41 +0100

    created Toolbar component + added Logo + created NavigationItems and NavigationItem components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
+
 import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => {
   return (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const Toolbar = props => {
+  return (
+    <header className={classes.Toolbar}>
+      <div>MENU</div>
+      <Logo />
+      <NavigationItems />
+    </header>
+  );
+};
+
+export default Toolbar;
