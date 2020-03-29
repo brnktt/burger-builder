554186580807868f05dd73ea36780c527d68ea03
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-29 22:49:27 +0200

    created Chechout component with ChechoutSummary component inside

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
+          <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+
+export class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
+      meat: 1
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
