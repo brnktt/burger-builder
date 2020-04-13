cf9504cf550dc36e68104a69ee9d0e645c544cf9
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-04-13 20:58:54 +0200

    added some testing

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+configure({ adapter: new Adapter() });
+
+describe("<NavigationItems />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
+  it("Should render two <NavigationItem/> elements if not authenticated", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("Should render three <NavigationItem/> elements if authenticated", () => {
+    // const wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it("Should render <NavigationItem> Logout </NavigationItem> elements if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
+});
